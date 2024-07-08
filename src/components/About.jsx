824316a54c5567111857bc0799cafd217ccf2d41
Akkkshattt <jainakshat543@gmail.com>
2024-07-08 23:11:03 +0530

    Modified landing page with more animation and images with register component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from './Button'
 
 export default function About() {
   return (
     <section className='relative top-10 mb-9 flex items-center justify-center'>
       <p className='text-3xl font-bold'>We are currently working on this page</p>
+       
     </section>
   )
 }
